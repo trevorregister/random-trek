d027ac3132504e2c93db3d93c73e482bb91be499
trevorregister <trevor.register@gmail.com>
2024-08-26 15:38:15 -0400

    fix: card images included in build assets

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -1,7 +1,7 @@
 <template>
     <v-card
     @click="toggleIsFiltered(showTitle)"
-    :image="imgSrc"
+    :image="`src/assets/${imgSrc}`"
     height="200px"
     width="200px"
     :class="isFiltered ? 'opacity-50' : 'opacity-100'"
@@ -13,6 +13,19 @@ import {
     ref, 
     onBeforeMount, 
     defineEmits } from 'vue'
+
+//Forces Vite to recognize the images as assets and bundle them in the build
+import disc from '../assets/disc.png'
+import ds9 from '../assets/ds9.jpeg'
+import ent from '../assets/ent.jpg'
+import ld from '../assets/ld.jpg'
+import pic from '../assets/pic.jpg'
+import prod from '../assets/prod.png'
+import snw from '../assets/snw.jpg'
+import tas from '../assets/tas.jpg'
+import tng from '../assets/tng.jpg'
+import tos from '../assets/tos.jpg'
+import voy from '../assets/voy.jpg'
 
 const props = defineProps({
   showTitle: String,
@@ -26,37 +39,37 @@ const isFiltered = ref(false)
 const getImage = (showTitle) => {
     switch(showTitle) {
         case 'The Next Generation':
-            imgSrc.value = '/assets/tng.jpg'
+            imgSrc.value = 'tng.jpg'
             break
         case 'Deep Space Nine':
-            imgSrc.value = '/assets/ds9.jpeg'
+            imgSrc.value = 'ds9.jpeg'
             break
         case 'Enterprise':
-            imgSrc.value = '/assets/ent.jpg'
+            imgSrc.value = 'ent.jpg'
             break
         case 'The Original Series':
-            imgSrc.value = '/assets/tos.jpg'
+            imgSrc.value = 'tos.jpg'
             break
         case 'Discovery':
-            imgSrc.value = '/assets/disc.png'
+            imgSrc.value = 'disc.png'
             break
         case 'Voyager':
-            imgSrc.value = '/assets/voy.jpg'
+            imgSrc.value = 'voy.jpg'
             break
         case 'Lower Decks':
-            imgSrc.value = './assets/ld.jpg'
+            imgSrc.value = 'ld.jpg'
             break
         case 'Strange New Worlds':
-            imgSrc.value = './assets/snw.jpg'
+            imgSrc.value = 'snw.jpg'
             break
         case 'Animated Series':
-            imgSrc.value = './assets/tas.jpg'
+            imgSrc.value = 'tas.jpg'
             break
         case 'Picard':
-            imgSrc.value = './assets/pic.jpg'
+            imgSrc.value = 'pic.jpg'
             break
         case 'Prodigy':
-            imgSrc.value = './assets/prod.png'
+            imgSrc.value = 'prod.png'
             break
     }
 }
