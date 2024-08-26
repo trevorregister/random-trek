76a22f7ae2589bdf7ee032ddc9125986311c0ec3
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-08-26 13:42:17 -0400

    fix: card images

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -26,37 +26,37 @@ const isFiltered = ref(false)
 const getImage = (showTitle) => {
     switch(showTitle) {
         case 'The Next Generation':
-            imgSrc.value = 'src/assets/tng.jpg'
+            imgSrc.value = './assets/tng.jpg'
             break
         case 'Deep Space Nine':
-            imgSrc.value = 'src/assets/ds9.jpeg'
+            imgSrc.value = './assets/ds9.jpeg'
             break
         case 'Enterprise':
-            imgSrc.value = 'src/assets/ent.jpg'
+            imgSrc.value = './assets/ent.jpg'
             break
         case 'The Original Series':
-            imgSrc.value = 'src/assets/tos.jpg'
+            imgSrc.value = './assets/tos.jpg'
             break
         case 'Discovery':
-            imgSrc.value = 'src/assets/disc.png'
+            imgSrc.value = './assets/disc.png'
             break
         case 'Voyager':
-            imgSrc.value = 'src/assets/voy.jpg'
+            imgSrc.value = './assets/voy.jpg'
             break
         case 'Lower Decks':
-            imgSrc.value = 'src/assets/ld.jpg'
+            imgSrc.value = './assets/ld.jpg'
             break
         case 'Strange New Worlds':
-            imgSrc.value = 'src/assets/snw.jpg'
+            imgSrc.value = './assets/snw.jpg'
             break
         case 'Animated Series':
-            imgSrc.value = 'src/assets/tas.jpg'
+            imgSrc.value = './assets/tas.jpg'
             break
         case 'Picard':
-            imgSrc.value = 'src/assets/pic.jpg'
+            imgSrc.value = './assets/pic.jpg'
             break
         case 'Prodigy':
-            imgSrc.value = 'src/assets/prod.png'
+            imgSrc.value = './assets/prod.png'
             break
     }
 }
